acd19a938c259dad43fcee228864d7694f167291
Stuart Hendren <stuart@committed.io>
2019-12-04 17:40:59 +0000

    Add scroll into view for active list item
    This is a partial fix of the sidebar presevation with the additional
    benifit or scrolling the active sidebar component into view when
    navigating directly or via the keyboard.
    
    fixes #12

--- theme/src/components/TreeNode.tsx
+++ theme/src/components/TreeNode.tsx
@@ -21,67 +21,94 @@ export interface TreeNodeProps extends Item {
   id: string
 }
 
-export const TreeNode = ({
-  id,
-  isActive,
-  navigate,
-  setCollapsed,
-  collapsed,
-  level,
-  label,
-  info,
-  items,
-  ...rest
-}: TreeNodeProps) => {
-  const theme = useTheme<Theme>()
-  const url = firstUrl({ items, info })
-  const isCollapsed = collapsed[id] || false
-  const hasChildren = items.length !== 0
-  const title = info ? info.title : label
-  const active = isActive(url)
-  return (
-    <>
-      <ListItem
-        style={{
-          paddingLeft: theme.spacing(2) * (level + 1)
-        }}
-        selected={active}
-        button
-        onClick={() => {
-          setCollapsed(id)
-          navigate(url)
-        }}
-      >
-        <ListItemText primary={title} />
-        {title && hasChildren ? (
-          <>
-            {!isCollapsed ? (
-              <Icons.KeyboardArrowDown />
-            ) : (
-              <Icons.KeyboardArrowRight />
-            )}
-          </>
-        ) : null}
-      </ListItem>
+class Scroller extends React.Component<{ active: boolean }> {
+  private ref = React.createRef<HTMLDivElement>()
 
-      {hasChildren ? (
-        <Collapse in={!isCollapsed} timeout="auto" unmountOnExit>
-          <List dense disablePadding>
-            {items.map((item, index) => (
-              <TreeNode
-                key={`${item.label}-${index}`}
-                isActive={isActive}
-                navigate={navigate}
-                setCollapsed={setCollapsed}
-                collapsed={collapsed}
-                level={level + 1}
-                {...item}
-              />
-            ))}
-          </List>
-        </Collapse>
-      ) : null}
-    </>
-  )
+  scroll = () => {
+    const { active } = this.props
+
+    if (active) {
+      this.ref.current.scrollIntoView({ block: 'center', inline: 'nearest' })
+    }
+  }
+
+  componentDidMount() {
+    this.scroll()
+  }
+
+  componentDidUpdate() {
+    this.scroll()
+  }
+
+  render() {
+    return <div ref={this.ref}>{this.props.children}</div>
+  }
 }
+
+export const TreeNode = React.memo(
+  ({
+    id,
+    isActive,
+    navigate,
+    setCollapsed,
+    collapsed,
+    level,
+    label,
+    info,
+    items,
+    ...rest
+  }: TreeNodeProps) => {
+    const ref = React.useRef()
+    const theme = useTheme<Theme>()
+    const url = firstUrl({ items, info })
+    const isCollapsed = collapsed[id] || false
+    const hasChildren = items.length !== 0
+    const title = info ? info.title : label
+    const active = isActive(url)
+    return (
+      <Scroller active={active}>
+        <ListItem
+          style={{
+            paddingLeft: theme.spacing(2) * (level + 1)
+          }}
+          selected={active}
+          button
+          onClick={() => {
+            setCollapsed(id)
+            navigate(url)
+          }}
+        >
+          <ListItemText primary={title} />
+          {title && hasChildren ? (
+            <>
+              {!isCollapsed ? (
+                <Icons.KeyboardArrowDown />
+              ) : (
+                <Icons.KeyboardArrowRight />
+              )}
+            </>
+          ) : null}
+        </ListItem>
+
+        {hasChildren ? (
+          <Collapse in={!isCollapsed} timeout="auto" unmountOnExit>
+            <List dense disablePadding>
+              {items.map((item, index) => (
+                <TreeNode
+                  key={`${item.label}-${index}`}
+                  isActive={isActive}
+                  navigate={navigate}
+                  setCollapsed={setCollapsed}
+                  collapsed={collapsed}
+                  level={level + 1}
+                  {...item}
+                />
+              ))}
+            </List>
+          </Collapse>
+        ) : null}
+      </Scroller>
+    )
+  }
+)
 export default TreeNode
