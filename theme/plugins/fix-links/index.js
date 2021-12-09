f1da1bfc045866f9b06bfde7da2e0b0b4d4def13
Stuart Hendren <stuart@committed.io>
2021-12-09 13:17:24 +0000

    feat(ci): adds new release process
    Uses semantic-release to orchistrate the build releases - added as feature to up the build
    
    fix #55

--- theme/plugins/fix-links/index.js
+++ theme/plugins/fix-links/index.js
@@ -1,7 +1,6 @@
 const visit = require('unist-util-visit')
 module.exports = ({ markdownAST }, pluginOptions) => {
   const protocols = ['mailto:', 'http://', 'ftp:', 'https://']
-
   visit(markdownAST, 'link', (node) => {
     // Any link we find which isn't internal, then ignore it
     if (protocols.find((p) => node.url.startsWith(p))) {
